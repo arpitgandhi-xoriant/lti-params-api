970d00cc64a6d296cc1e9773f0f5d8fe4f6a4a84
Ritesh Chouhan <Ritesh.Chouhan@Xoriant.com>
2022-06-03 11:29:10 +0000

    Get LTI info from usage_id and response it in a dict

--- urls.py
+++ urls.py
@@ -1,17 +1,19 @@
 """
 URL definitions for the lti_params_api app.
 """
-
-
 from django.conf.urls import url
 
-from cms.djangoapps.lti_params_api import views
+from .views import LTIParams
 
+
+USAGE_ID_PATTERN = r'(?P<usage_id>(?:i4x://?[^/]+/[^/]+/[^/]+/[^@]+(?:@[^/]+)?)|(?:[^/]+))'
 
 urlpatterns = [
     url(
-        r'^lti_params_list/$',
-        views.LTIParams.as_view(),
+        r'^lti_params_list/{usage_key}/$'.format(
+            usage_key=USAGE_ID_PATTERN
+        ),
+        LTIParams.as_view(),
         name="get_lti_params_list"
     ),
 ]
--- views.py
+++ views.py
@@ -1,11 +1,32 @@
 """
-Views for the verification flow
+Views for the lti_params_api app.
 """
-
 from rest_framework.views import APIView
+from rest_framework.response import Response
+from xmodule.modulestore.django import modulestore
+
+from cms.djangoapps.contentstore.views.helpers import usage_key_with_run
 
 
 class LTIParams(APIView):
+    """
+    This class will handle API request and filter data.
+    """
+    def get(self, request, usage_id):
+        usage_key = usage_key_with_run(usage_id)
 
-    def get(self, request):
-        pass
+        module_store = modulestore()
+        lti_info = module_store.get_item(usage_key)
+
+        lti_info_dict = {}
+        lti_info_dict["block_key"] = usage_id
+        lti_info_dict["display_name"] = lti_info.display_name
+        lti_info_dict['launch_url'] = lti_info.launch_url
+        lti_info_dict['tool_id'] = str(lti_info.tool_id)
+        lti_info_dict['custom_parameters'] = lti_info.custom_parameters
+        lti_info_dict['scored'] = lti_info.has_score
+        lti_info_dict['send_email'] = lti_info.ask_to_send_email
+        lti_info_dict['send_name'] = lti_info.ask_to_send_name
+        lti_info_dict['send_username'] = lti_info.ask_to_send_username
+
+        return Response([lti_info_dict])
